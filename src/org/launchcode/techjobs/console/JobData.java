cda483faed45fbddaed57428e8d10b1d4c125a15
lindseyms <51429410+lindseyms@users.noreply.github.com>
2021-01-27 21:20:53 -0600

    Created implemented bonus missions

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -48,8 +48,9 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> copy = new ArrayList<>(allJobs);
 
-        return allJobs;
+        return copy;
     }
 
     /**
@@ -72,9 +73,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
